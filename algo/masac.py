092be1fdea5de9a8f816774178edde11b445e75e
zhr <3117216418@qq.com>
2024-10-24 22:32:48 +0800

    fix bug

--- algo/masac.py
+++ algo/masac.py
@@ -20,10 +20,10 @@ class MASAC():
         alpha_loss_list, q_value_loss1_list, q_value_loss2_list, policy_loss_list = [], [], [], []
         for i1 in range(self.n_agents):
             alpha_loss, q_value_loss1, q_value_loss2, policy_loss = self.model[i1].train(batch_size, reward_scale=1,  auto_entropy=auto_entropy, target_entropy=-1. * self.action_dim)
-            alpha_loss_list.append(alpha_loss)
-            q_value_loss1_list.append(q_value_loss1)
-            q_value_loss2_list.append(q_value_loss2)
-            policy_loss_list.append(policy_loss)
+            alpha_loss_list.append(alpha_loss.cpu().detach())
+            q_value_loss1_list.append(q_value_loss1.cpu().detach())
+            q_value_loss2_list.append(q_value_loss2.cpu().detach())
+            policy_loss_list.append(policy_loss.cpu().detach())
 
         return np.sum(alpha_loss_list), np.sum(q_value_loss1_list), np.sum(q_value_loss2_list), np.sum(policy_loss_list)
 
